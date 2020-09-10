18e6a30dfff3348e064e9defb630d7b31d2f7ef4
hyj111 <1069234622@qq.com>
2020-09-10 09:45:13 +0800

    静态页面的初步完成

---
+++ src/views/amap/amap.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="amap-wrap">
+    <el-amap vid="amapContainer" :zoom="zoom" :events="events" class="amap-demo"></el-amap>
+  </div>
+</template>
+
+<script>
+import { AMapManager, lazyAMapApiLoaderInstance } from "vue-amap";
+export default {
+  name: "amap",
+  data() {
+    return {
+      map: null,
+      zoom: 14,
+      events: {},
+    };
+  },
+  mounted() {
+    lazyAMapApiLoaderInstance.load().then(() => {
+      // your code ...
+      this.map = new AMap.Map("amapContainer", {
+        center: new AMap.LngLat(116.403433, 39.903899),
+        zoom: this.zoom,
+      });
+    });
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.amap-wrap {
+  height: 100vh;
+}
+</style>
+
---
+++ src/views/safe/password.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="user-container">
+    <back>
+      <span slot="center">修改登录密码</span>
+    </back>
+    <div class="cars-form-ui">
+      <el-form ref="form" :model="form">
+        <el-form-item>
+          <el-input v-model="form.name" placeholder="原始密码"></el-input>
+        </el-form-item>
+         <el-form-item>
+          <el-input v-model="form.name" placeholder="新密码"></el-input>
+        </el-form-item>
+         <el-form-item>
+          <el-input v-model="form.name" placeholder="确认密码"></el-input>
+        </el-form-item>
+         <el-form-item>
+             <button class="button-vcode" type="button">获取验证码</button>
+          <el-input v-model="form.name" placeholder="验证码"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit" class="button-black">确定</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+import Back from "../../components/back/back";
+export default {
+  name: "Password",
+  components: {
+    Back,
+  },
+  data() {
+    return {
+      form: {
+        name: "",
+        region: "",
+        date1: "",
+        date2: "",
+        delivery: false,
+        type: [],
+        resource: "",
+        desc: "",
+      },
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log("submit!");
+    },
+  },
+};
+</script>
+
+<style>
+</style>
